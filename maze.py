6a1cd48dc497a3defef321bbef38753047fcb51f
Robot Xia <liuminxiahongze@163.com>
2020-03-28 21:13:08 +0800

    matrix

--- maze.py
+++ maze.py
@@ -14,4 +14,24 @@ import matplotlib.cm as cm
 from collections import deque
 
  
+rows = int(input("Width: "))
+cols = int(input("Length: "))
+lenminus=random.randint(cols//4,cols//3)
+widminus=random.randint(rows//4,cols//3)
+ 
+
+M = np.zeros((rows,cols,5), dtype=np.uint8)
+image = np.zeros((rows*10,cols*10), dtype=np.uint8)
+x = 0
+y = 0
+prev = [(x,y)]
+for l in range(rows-lenminus,rows):
+    for m in range(widminus):
+        for n in range (5):
+            M[l,m,n]=9
+for p in range(lenminus):
+    for q in range(cols-widminus,cols):
+        for r in range (5):
+            M[p,q,r]=9
+
 
